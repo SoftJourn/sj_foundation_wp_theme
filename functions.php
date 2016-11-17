8c6d7afdc579bad5dc3b4a4987bbd417af7ac7b2
Denys Fitsak <dfitsak@softjourn.com>
2016-11-17 12:32:04 +0200

    set wp nonce

--- functions.php
+++ functions.php
@@ -1,8 +1,17 @@
 <?php
 
 add_action( 'wp_enqueue_scripts', 'wp_sj_enqueue' );
+add_theme_support( 'post-thumbnails' );
 
 function wp_sj_enqueue() {
     wp_enqueue_script( 'wp-api' );
-    wp_localize_script( 'wp-api', 'wpApiSettings', array( 'root' => esc_url_raw( rest_url() ), 'nonce' => wp_create_nonce( 'wp_rest' ) ) );
+    wp_localize_script(
+        'wp-api',
+        'wpApiSettings',
+        array(
+            'root' => esc_url_raw( rest_url() ),
+            'nonce' => wp_create_nonce( 'wp_rest' ),
+            'user' => wp_get_current_user(),
+        )
+    );
 }
