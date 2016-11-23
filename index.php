86b85a712d62b542d26b5546c686d387bf234f3d
Denys Fitsak <dfitsak@softjourn.com>
2016-11-23 09:38:10 +0200

    remove bootstrap urls

--- index.php
+++ index.php
@@ -3,15 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <title>Projects</title>
+        <title>SJ Foundation</title>
         <?php wp_head(); ?>
     </head>
     <body>
         <div id="root"></div>
         <?php wp_footer(); ?>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <script src="http://sj-foundation-static.testing.softjourn.if.ua/app.bundle.js"></script>
-
+<!--        <script src="http://localhost:3000/assets/dist/app.bundle.js"></script>-->
     </body>
 </html>
