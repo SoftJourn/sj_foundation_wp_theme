132982b060b18e9be31c31ab8c2f0c2deca933a9
Denys Fitsak <dfitsak@gmail.com>
2017-03-08 15:26:42 +0100

    add tinymce script

--- functions.php
+++ functions.php
@@ -3,7 +3,12 @@
 add_action( 'wp_enqueue_scripts', 'wp_sj_enqueue' );
 add_theme_support( 'post-thumbnails' );
 
+
+
 function wp_sj_enqueue() {
+    wp_register_script('tinymce_editor', 'https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=rbhbxh16l5mmnokwzcb9anc5zdh2pf9bzizjrgc8r0jcta0q', false);
+    wp_enqueue_script("tinymce_editor");
+
     wp_enqueue_script( 'wp-api' );
     wp_localize_script(
         'wp-api',
